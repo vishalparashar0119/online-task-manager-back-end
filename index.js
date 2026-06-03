89f06341703110dca8712411f004cd2d7bc4d2fd
vishalparashar0119 <itsvishalparashar0119@gmail.com>
2026-06-03 14:56:48 +0530

    setup some basic middle ware

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 const port = 3001;
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
     return res.status(200).json({
